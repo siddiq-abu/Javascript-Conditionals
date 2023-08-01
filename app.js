e45483ee2ab46d8febceb097feb6aaa01bd6fc15
siddiq-abu~ <abusiddiq200g3@gmail.com>
2023-08-01 11:52:07 +0000

    update

--- app.js
+++ app.js
@@ -46,6 +46,100 @@ else {
     console.log(`No enough money,you own ${difference * -1} amount `);
 }
 
+// / Logical Operator /// {&& AND ||} HOW TO USE THEM ///
+
+// This For && //
+
+let money = 50
+let amount =30
+let isStoreOpen = true
+
+if (money >= amount && isStoreOpen === true){
+    console.log(`Print The Receipt`);
+}
+
+// This For || //
+
+let carPrice = 100
+let cashAmount = 150
+let isStoreOpened = false
+
+if (cashAmount >= carPrice || isStoreOpened === false) {
+    console.log(`Print The Receipt`);
+}
+
+/// HOW FALSY AND TRUTHY VALUE WORKS ///
+
+// under falsy //
+
+let tomatoes = 15
+let cashOnYou = 20
+let isShopOpen = false
+
+if ("") {
+    console.log(`true`);
+}
+
+else{
+    console.log(`falsy value`);
+}
+
+
+// under truthy //
+
+let haircut = 10
+let cashOnMe = 20
+let barberingShopIsOpen = true 
+
+if ("o"){
+    console.log(`truthy value`);
+}
+
+else{
+    console.log(`falsy values`);
+}
+
+// How to check weather your values are truthy or falsy //
+// under falsy //
+let val = "";
+if (val){
+    console.log(`truthy val`);
+}
+
+else{
+console.log(`falsy val`);
+}
+
+// under truthy //
+
+let value = "o";
+
+if (value) {
+    console.log(`truthy`);
+}
+
+else{
+    console.log(`falsy`);
+}
+
+
+/// TERNARY OPERATORS ///
+
+//  A shortcut for an if else statement //
+// EG1//
+
+let hot = true
+
+hot? console.log(`weather is hot outside`): console.log(`weather is cold`);
+
+//EG2//
+
+let subscribed = true
+let loggedInn = true
+let str = subscribed && loggedInn ?
+`show the video` : `hide the video`
+console.log(str); 
+
 
 
 
